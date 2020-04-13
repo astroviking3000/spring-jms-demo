ce6b0080a71fcc0c0c752be1df4197b3a1f646f0
Tyler Gray <tyler.gray@gmail.com>
2020-04-12 22:03:39 -0500

    Removed embedded artemis server.  Uses external MS.

--- src/main/java/com/astroviking/springjmsdemo/SpringJmsDemoApplication.java
+++ src/main/java/com/astroviking/springjmsdemo/SpringJmsDemoApplication.java
@@ -1,28 +1,12 @@
 package com.astroviking.springjmsdemo;
 
-import org.apache.activemq.artemis.core.config.impl.ConfigurationImpl;
-import org.apache.activemq.artemis.core.server.ActiveMQServer;
-import org.apache.activemq.artemis.core.server.ActiveMQServers;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.lang.module.Configuration;
 
 @SpringBootApplication
 public class SpringJmsDemoApplication {
 
-  public static void main(String[] args) throws Exception {
-    ActiveMQServer activeMQServer =
-        ActiveMQServers.newActiveMQServer(
-            new ConfigurationImpl()
-                .setPersistenceEnabled(false)
-                .setJournalDirectory("target/data/journal")
-                .setSecurityEnabled(false)
-            //        .addAcceptorConfiguration("invm", "vm://0")
-            );
-
-    activeMQServer.start();
-
+  public static void main(String[] args) {
     SpringApplication.run(SpringJmsDemoApplication.class, args);
   }
 }
